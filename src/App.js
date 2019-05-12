448d25be78cddefa36f61c7924746ebf0e1030bb
conqsolid <conqsolid@gmail.com>
2019-05-12 16:12:21 +0300

    functional component person added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
 
     //With jsx
-    // return (
-    //   <div className="App">
-    //    <h1>Hello I'm a React App</h1>
-    //    </div>
-    // );
+    return (
+      <div className="App">
+        <h1>Hello I'm a React App</h1>
+        <p>This really works.</p>
+        <Person />
+      </div>
+    );
 
     //Wihhout jsx
-    return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I\'m a React App.'));
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'I\'m a React App.'));
   }
 }
 
