354bb6f53e916ba54ba7f3e9b4528da385cfc2ea
conqsolid <conqsolid@gmail.com>
2019-05-19 01:07:39 +0300

    added global deneme class

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import cssClasses from './Person.css';
 
-
 const person = (props) => {
     return (
         <div className={cssClasses.Person}>
-            <p>I'm {props.name} and I'm at {props.age} years old.</p>
+            <p className="deneme">I'm {props.name} and I'm at {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.change} value={props.name} />
             <button onClick={props.clickDelete}>Delete Object</button>
