49b47f6cfb98997eeaa7dd7de2fb53f4e8c97f1f
conqsolid <conqsolid@gmail.com>
2019-08-24 16:04:29 +0300

    revert
    revert

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -19,6 +19,17 @@ class Person extends Component {
                 <button onClick={this.props.clickDelete}>Delete Object</button>
             </div>)
     }
+=======
+const person = (props) => {
+    return (
+        <div className={cssClasses.Person}>
+            <p className="deneme">I'm {props.name} and I'm at {props.age} years old.</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.change} value={props.name} />
+            <button onClick={props.clickDelete}>Delete Object</button>
+        </div>
+    )
+>>>>>>> parent of ada3f88... Component lifecycle methods
 };
 
 export default Person;
